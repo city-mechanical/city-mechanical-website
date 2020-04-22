3b26c3487e17079c47a4372adfabf87459ace36a
William Crutchfield <william.r.crutchfield@gmail.com>
2020-04-21 23:00:12 -0400

    Migrate to Google PDF Viewer

--- components/PDFViewer.js
+++ components/PDFViewer.js
@@ -7,14 +7,22 @@ function PDFViewer(props) {
       style={({ position: "absolute" }, { left: 0 }, { top: 0 })}
       width="100%"
       height="100%"
-      data={file}
+      data={
+        "https://docs.google.com/viewer?url=https://www.citymechanical.com" +
+        file +
+        "&embedded=true"
+      }
       type="application/pdf"
     >
       <embed
         style={({ position: "absolute" }, { left: 0 }, { top: 0 })}
         width="100%"
         height="100%"
-        src={file}
+        src={
+          "https://docs.google.com/viewer?url=https://www.citymechanical.com" +
+          file +
+          "&embedded=true"
+        }
         type="application/pdf"
       ></embed>
     </object>
